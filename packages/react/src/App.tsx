1424f1ab59f4716236d6c8a2bf5bfef3fb7fbfbf
Aditya Agarwal <adityaa803@gmail.com>
2022-05-22 11:56:36 +0530

    initial boilerplate

---
+++ packages/react/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      React app
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/solid/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      Solid app
+    </div>
+  );
+};
+
+export default App;
