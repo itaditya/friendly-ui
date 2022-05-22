b810d2c11f44bce457ece65bda218ae6746d1726
Aditya Agarwal <adityaa803@gmail.com>
2022-05-22 14:14:00 +0530

    split card bottom

--- packages/solid/src/App.tsx
+++ packages/solid/src/App.tsx
@@ -1,18 +1,27 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal, Show } from 'solid-js';
 import layoutStyles from '@friendly-ui/design/layout.module.css';
 import cardStyles from '@friendly-ui/design/card.module.css';
 
 const App: Component = () => {
+  const [show, setShow] = createSignal(false);
+
   return (
     <div class={layoutStyles.layoutApp}>
       <section>
         <input type="text" placeholder="Search" />
+        <button onClick={() => setShow((old) => !old)}>Toggle</button>
+        <Show when={show()}>
+          <div>
+            <p>Recent Searches</p>
+            <br />
+          </div>
+        </Show>
       </section>
       <section class={`${cardStyles.card} ${layoutStyles.fillChild}`}>
         Hello
       </section>
       <section class={cardStyles.card} data-variant="split">
-        Hello
+        Select a person to see their details here
       </section>
     </div>
   );
