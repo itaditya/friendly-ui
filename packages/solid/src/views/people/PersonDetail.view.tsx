0a4c1d6883e568db89ed111aad3bd43e734c577b
Aditya Agarwal <adityaa803@gmail.com>
2022-05-28 02:06:40 +0530

    basic add friend btn in details

--- packages/solid/src/views/people/PersonDetail.view.tsx
+++ packages/solid/src/views/people/PersonDetail.view.tsx
@@ -1,13 +1,57 @@
-import { Component } from 'solid-js';
+import personDetailStyles from '@friendly-ui/design/person_detail.module.css';
 import { Navigate, useParams } from 'solid-app-router';
 import { autoAnimate } from 'solid-auto-animate';
+import { Component, Match, Switch } from 'solid-js';
+import { useFriendsStore } from '_shared/friendsStore';
+import { PersonId, RequestStatus } from '_shared/types';
 import { usePeopleRouteData } from './primitives';
-import personDetailStyles from '@friendly-ui/design/person_detail.module.css';
+
+interface AddFriendHandlerData {
+  id: PersonId;
+  status: RequestStatus;
+}
 
 const PersonDetailView: Component = () => {
   const params = useParams();
   const routeData = usePeopleRouteData();
+  const [friends, methods] = useFriendsStore();
   autoAnimate; // to prevent TS from removing the directive
+
+  function friendStatus(personId: PersonId) {
+    return friends.statusMap[personId];
+  }
+
+  function handleAddFriend(data: AddFriendHandlerData, event: MouseEvent) {
+    const { id, status } = data;
+    if (status === 'requested') {
+      event.preventDefault();
+      return;
+    }
+
+    if (status === 'accepted') {
+      methods.removeFriend(id);
+      return;
+    }
+
+    methods.addFriend(id);
+  }
+
+  function renderAddBtn(id: PersonId) {
+    const status = friendStatus(id);
+
+    return (
+      <button
+        class={personDetailStyles.addBtn}
+        type="button"
+        onClick={[handleAddFriend, { id, status }]}
+      >
+        <Switch fallback="Add Friend">
+          <Match when={friendStatus(id) === 'requested'}>Pending</Match>
+          <Match when={friendStatus(id) === 'accepted'}>Remove Friend</Match>
+        </Switch>
+      </button>
+    );
+  }
 
   function renderPerson() {
     const fetchedPeople = routeData();
@@ -22,19 +66,17 @@ const PersonDetailView: Component = () => {
       return <Navigate href="/people" />;
     }
 
-    const { name, description, imageUrl } = person;
+    const { id, name, description, imageUrl } = person;
 
     return (
       <>
         <div>
           <img src={imageUrl} alt="" class={personDetailStyles.avatar} />
         </div>
-        <div class={personDetailStyles.bio}>
+        <div class={personDetailStyles.bio} use:autoAnimate>
           <h2 class={personDetailStyles.name}>{name}</h2>
           <p class={personDetailStyles.description}>{description}</p>
-          <button class={personDetailStyles.addBtn} type="button">
-            Add Friend
-          </button>
+          {renderAddBtn(id)}
         </div>
       </>
     );
