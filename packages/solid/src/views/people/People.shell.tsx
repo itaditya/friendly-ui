4b78651e74b5ee6b1ca276c2a51e90ed51d579b4
Aditya Agarwal <adityaa803@gmail.com>
2022-05-28 02:17:53 +0530

    friends filter & btn styles

--- packages/solid/src/views/people/People.shell.tsx
+++ packages/solid/src/views/people/People.shell.tsx
@@ -1,4 +1,4 @@
-import { Component, For, Show } from 'solid-js';
+import { Component, For } from 'solid-js';
 import { Link, Outlet, useSearchParams } from 'solid-app-router';
 import { autoAnimate } from 'solid-auto-animate';
 import PersonAvatar from './PersonAvatar';
@@ -16,13 +16,14 @@ const PeopleView: Component = () => {
       <section class={`${peopleStyles.listCard} ${peopleStyles.card}`}>
         <header class={peopleStyles.header}>
           <h4 class={peopleStyles.heading}>Connect</h4>
-          <Link href={headerHref()} class={peopleStyles.headerAction}>
-            <Show
-              when={searchParams.filter === 'friends'}
-              fallback={'Show Friends'}
-            >
-              Show all people
-            </Show>
+          <Link
+            href={headerHref()}
+            class={peopleStyles.headerAction}
+            classList={{
+              [peopleStyles.activeFilter]: searchParams.filter === 'friends',
+            }}
+          >
+            Just Friends
           </Link>
         </header>
         <main class={peopleStyles.avatarGrid} use:autoAnimate>
