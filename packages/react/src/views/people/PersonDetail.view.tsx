3f4ba654088b24e7dc31d8480a4ccecdf5898673
Aditya Agarwal <adityaa803@gmail.com>
2022-05-29 13:52:33 +0530

    person detail

--- packages/react/src/views/people/PersonDetail.view.tsx
+++ packages/react/src/views/people/PersonDetail.view.tsx
@@ -1,5 +1,31 @@
+import personDetailStyles from '@friendly-ui/design/person_detail.module.css';
+import { useMatch } from '@tanstack/react-location';
+
 function PersonDetailView() {
-  return <div>PersonDetailView</div>;
+  const {
+    params: { id },
+    data: { people },
+  } = useMatch();
+
+  const person = people.find((person) => person.id === id);
+  const { name, description, imageUrl } = person;
+
+  return (
+    <div className={personDetailStyles.wrapper}>
+      <div>
+        <img src={imageUrl} alt="" className={personDetailStyles.avatar} />
+      </div>
+      <div className={personDetailStyles.bio}>
+        <h2 className={personDetailStyles.name}>{name}</h2>
+        <p className={personDetailStyles.description}>{description}</p>
+        {/* <AddBtn
+          personId={id}
+          status={friendStatus(id)}
+          onClick={[handleAddFriend, id]}
+        /> */}
+      </div>
+    </div>
+  );
 }
 
 export default PersonDetailView;
