f4980693b03085bf5a07ab75fadfdebb46f7226a
Aditya Agarwal <adityaa803@gmail.com>
2022-06-02 11:40:44 +0530

    home page done

--- packages/react/src/views/Home.view.tsx
+++ packages/react/src/views/Home.view.tsx
@@ -1,5 +1,20 @@
+import homeStyles from '@friendly-ui/design/home.module.css';
+import { Link } from '@tanstack/react-location';
+
 function HomeView() {
-  return <div>HomeView</div>;
+  return (
+    <div className={homeStyles.wrapper}>
+      <Link className={homeStyles.demoLink} to="/people">
+        React Demo
+      </Link>
+      <a
+        className={homeStyles.demoLink}
+        href="https://friendly-ui-solid.vercel.app/people"
+      >
+        Solid Demo
+      </a>
+    </div>
+  );
 }
 
 export default HomeView;
